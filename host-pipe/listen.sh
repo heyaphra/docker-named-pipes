985b6e327ac3a04d296c9c11cea099e9bc22d7e0
spidercatnat <bloomjazzer@gmail.com>
2021-06-05 16:35:54 -0700

    Adds working listen.sh

--- host-pipe/listen.sh
+++ host-pipe/listen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOST_PIPE_PATH = "~/repos/docker-named-pipes/host-pipe/cmd-runner"
-OUTPUT_PATH = "~/repos/docker-named-pipes/host-pipe/output.txt"
+HOST_PIPE_PATH="/home/natalie/repos/docker-named-pipes/host-pipe/cmd-runner"
+OUTPUT_PATH="/home/natalie/repos/docker-named-pipes/host-pipe/output.txt"
 
 while true; do eval "$(cat $HOST_PIPE_PATH)" &>$OUTPUT_PATH; done
