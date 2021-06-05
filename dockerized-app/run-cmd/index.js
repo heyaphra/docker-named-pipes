d6d20217b5ae9ea3e82d82ab21a324327d379441
spidercatnat <bloomjazzer@gmail.com>
2021-06-05 16:09:25 -0700

    rm typo in pipe path

--- dockerized-app/run-cmd/index.js
+++ dockerized-app/run-cmd/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const pipePath = "./host_pipe/cmd_runner";
+const pipePath = "/host_pipe/cmd_runner";
 const outputPath = "~/repos/docker-named-pipes/output.txt";
 const cmd = `echo "this command was executed on the host from the container! > ${outputPath}"`;
 
